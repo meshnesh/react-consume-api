396914db071cbf5c8a70bf73d6d0ed78196c1991
meshnesh <tonnie.nesh@gmail.com>
2018-02-22 23:23:46 +0300

    [Feature] Update component to populate api data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { connect } from 'react-redux';
 
 // components
 import EventList from './Events/EventList';
+import { fetchEvents } from '../actions/eventActions';
+
+@connect((state) => {
+  // console.log("ins store",store.eventsReducer.events);
+  return {
+    events: state.eventsReducer.events
+  }
+})
 
 class Main extends Component {
+  componentWillMount(){
+
+    this.props.dispatch(fetchEvents());
+  }
+
   render() {
+    console.log("From the main file ",this.props.events);
     return (
       <div>
-        <EventList />
+        <EventList events ={this.props.events}/>
       </div>
     )
   }
