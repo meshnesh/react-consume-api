3dc2a22c874a51fbbb0eb155af4d98baba16cecc
meshnesh <tonnie.nesh@gmail.com>
2018-02-22 20:58:40 +0300

    [Feature] Import redux store and setup provider

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import { Provider } from "react-redux";
 
 //components
 import MainComponent from "./components/Main.js";
+import store from './store.js'; 
 
 ReactDOM.render(
-    <MainComponent />
+    <Provider store={ store }>
+        <MainComponent />
+    </Provider>
     ,document.getElementById("main")
 );
