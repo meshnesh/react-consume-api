ae303a2639d518cfeac3e4322c19e525b191d1b7
meshnesh <tonnie.nesh@gmail.com>
2018-02-22 23:25:52 +0300

    [Feature] Create eventnodes for displaying data from the props

--- src/components/Events/EventList.js
+++ src/components/Events/EventList.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
 
+import Event from './Event';
+
 class EventList extends Component {
   render(){
+    if(Object.getOwnPropertyNames(this.props.events).length === 0){
+      return (<div></div>)
+    }
+    var eventNodes = this.props.events.map( function( event ) {
+      return (
+        <Event title={ event.title } location={ event.location }
+        key={ event.id } date={ event.date }>
+        {event.title}
+        </Event>
+      )
+    });
     return (
       <div>
-        Event List Component
+        <div>
+          <center>
+            <h1 className="event-List">Event List</h1>
+            {eventNodes}
+          </center>
+        </div>
       </div>
     )
   }
