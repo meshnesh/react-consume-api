db6a00e53f9e871dbce5a77967982e6291b96e6f
meshnesh <tonnie.nesh@gmail.com>
2018-02-23 00:46:44 +0300

    [Feature] Created Footer router

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="page-footer">
+                <div className="footer-copyright">
+                    <div className="container center">
+                        © 2017 Bright Events
+                    </div>
+                </div>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
