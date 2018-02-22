3609a8093d1e8951d2b0008e6013a2695a86ee60
meshnesh <tonnie.nesh@gmail.com>
2018-02-22 23:22:36 +0300

    [Feature] Make ajax requests to fetch data

--- src/actions/eventActions.js
+++ src/actions/eventActions.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-export function fetchEvents(){
+export function fetchEvents() {
 
-  return function(dispatch){
+  return function(dispatch) {
     axios.get('https://bright-events.herokuapp.com/api/events/all')
-        .then((response) =>{
-          dispatch({type:'FETCH_USERS_FULFILLED', payload:response.data.data});
+        .then( (response) =>{
+          dispatch({type:'FETCH_EVENTS_FULFILLED', payload:response.data});
         })
-        .catch((err) => {
-          dispatch({type:'FETCH_USERS_REJECTED',payload:err})
+        .catch( (err) => {
+          dispatch({type:'FETCH_EVENTS_REJECTED',payload:err})
         })
   }
 }
