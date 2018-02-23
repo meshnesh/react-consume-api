cca68f6e92a6fd54ed713847b3c17596242ed0cb
meshnesh <tonnie.nesh@gmail.com>
2018-02-23 03:16:12 +0300

    [Bug] Fix about page routing

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+//components
 import Navigation from './Navigation/Navigation';
+import Footer from './Footer/Footer';
 
 export default class About extends React.Component {
 	render () {
