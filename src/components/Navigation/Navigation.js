643ec8866568dad1a6869de9aff8875ce97f00c2
meshnesh <tonnie.nesh@gmail.com>
2018-02-23 03:13:05 +0300

    [Feature] Create different accesible routes

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 
 class Nav extends Component {
@@ -6,9 +7,17 @@ class Nav extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a href="index.html" className="brand-logo">Bright Events</a>
+                    <a href="index.html" className="brand-logo">
+                    <Link to="/">
+                        Bright Events
+                    </Link>
+                    </a>
                     <ul id="nav-mobile hide-on-small-only" className="right hide-on-med-and-down">
-                        <li><a href="#"></a></li>
+                        <li><a href="#">
+                        <Link to="/about">
+                            About
+                        </Link>
+                        </a></li>
                         <li><a href="signup.html">Sign In</a></li>
                         <li><a href="manageTickets.html">Manage Events</a></li>
                     </ul>
