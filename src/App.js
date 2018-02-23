732fd30bdc384a947b6b750e1ec35d8161a23024
meshnesh <tonnie.nesh@gmail.com>
2018-02-23 03:11:05 +0300

    [Feature] Implement routing for the app in store provider

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
+// main routes
+import AppRoutes from './routes';
+
 //components
 import MainComponent from "./components/Main.js";
 import store from './store.js'; 
 
 ReactDOM.render(
     <Provider store={ store }>
-        <MainComponent />
+        <AppRoutes />
     </Provider>
     ,document.getElementById("main")
 );
