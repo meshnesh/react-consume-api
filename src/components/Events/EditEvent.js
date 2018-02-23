8a86f641563c3d3df4dc5a044acf4bf91ee65e2d
meshnesh <tonnie.nesh@gmail.com>
2018-02-23 04:30:57 +0300

    [Feature] Create new component for single event page view

---
+++ src/components/Events/EditEvent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+//components
+import Navigation from '../Navigation/Navigation';
+import Footer from '../Footer/Footer';
+
+export default class EditEvent extends Component {
+	render () {
+		return (
+			<div>
+                <Navigation />
+				    Single Event page data here
+                <Footer />
+			</div>
+		)
+	}
+}
