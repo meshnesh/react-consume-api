dc91344e990179b7e09cfe17fe19f2c142a4bb98
meshnesh <tonnie.nesh@gmail.com>
2018-02-23 04:31:29 +0300

    [Chore] Update route for single event view

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import App from './components/Main';
 import About from './components/About';
+import EditEvent from './components/Events/EditEvent';
 
 export default () => {
 	return (
@@ -9,6 +10,7 @@ export default () => {
 			<Switch>
 			  <Route exact path='/' component={App}/>
 			  <Route path='/about' component={About}/>
+              <Route exact path='/:id' component={EditEvent}/>
 			</Switch>
 		</BrowserRouter>
 	)
