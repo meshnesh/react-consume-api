7cccb27e15f2591dfd521ee3093eb5e3b76616db
meshnesh <tonnie.nesh@gmail.com>
2018-02-23 03:08:41 +0300

    [Feature] Create demo About page to test route

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Navigation from './Navigation/Navigation';
+
+export default class About extends React.Component {
+	render () {
+		return (
+			<div>
+                <Navigation />
+				About Page My N***a
+			</div>
+		)
+	}
+}
