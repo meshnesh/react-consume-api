233983f20ee701ffb8726e7aeb7d1acbd4e7cd82
meshnesh <tonnie.nesh@gmail.com>
2018-02-27 08:14:22 +0300

    [Feature] Create props to handle state of forms in login

--- src/components/Users/UserLogin.js
+++ src/components/Users/UserLogin.js
@@ -7,27 +7,46 @@ import Footer from '../Footer/Footer';
 
 
 class UserLogin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.id]: e.target.value });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("User Login", this.state);
+    }
     render() {
         return(
             <div>
                 <Navigation />
                 <div className="row rowParent">
-                    <form className="col s8 form">
+                    <form className="col s8 form" onSubmit={this.handleSubmit}>
 
                         {/* Email & Password form */}
                         <div className="row">
                             <div className="input-field col s6">
-                                <input id="email" type="email" className="validate"/>
+                                <input id="email" type="email" className="validate" value={this.state.email} onChange={this.handleChange}/>
                                 <label for="email">Email</label>
                             </div>
                             <div className="input-field col s6">
-                                <input id="password" type="password" className="validate"/>
+                                <input id="password" type="password" className="validate" value={this.state.password} onChange={this.handleChange}/>
                                 <label for="password">Password</label>
                             </div>
                         </div>
-                        <a className="waves-effect waves-light btn">
+                        <button className="waves-effect waves-light btn">
                             Submit
-                        </a>
+                        </button>
                         <p>
                             Don't have an account: <Link to="/register">Register</Link>
                         </p>
