6bf2227164d1de3287e1fd344887ecbad3524065
meshnesh <tonnie.nesh@gmail.com>
2018-02-27 07:08:15 +0300

    [Chore] Update routes for the app components

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import App from './components/Main';
 import About from './components/About';
+import UserRegistration from './components/Users/UserRegistration';
+import UserLogin from './components/Users/UserLogin';
 import EditEvent from './components/Events/EditEvent';
+
 
 export default () => {
 	return (
 		<BrowserRouter>
 			<Switch>
 			  <Route exact path='/' component={App}/>
-			  <Route path='/about' component={About}/>
-              <Route exact path='/:id' component={EditEvent}/>
+			  <Route exact path='/about' component={About}/>
+			  <Route exact path='/register' component={UserRegistration}/>
+			  <Route exact path='/login' component={UserLogin}/>
+			  <Route exact path='/:id' component={EditEvent}/>
 			</Switch>
 		</BrowserRouter>
 	)
