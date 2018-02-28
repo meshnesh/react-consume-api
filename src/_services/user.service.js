88bf9aac726ae1f4d5c355e1bbe2247bbd88b0d5
meshnesh <tonnie.nesh@gmail.com>
2018-02-28 20:24:35 +0300

    [Feature] Create axios base for http request

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -1,14 +1,22 @@
-const userService = { 
-	register(user) {
-		const requestOptions = {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(user)
-		};
+import axios from "axios";
+import store from '../store';
 
-		return fetch(
-			'https://bright-events.herokuapp.com/api/auth/register',
-			requestOptions).then(handleResponse
-		);
+const instance = axios.create({
+	baseURL:'https://bright-events.herokuapp.com',
+	timeout:5000,
+	headers:{ 
+		Accept:'application/json',
+		ContentType:'application/json'
 	}
-};
+})
+
+instance.interceptors.request.use(function(config){
+	const{ access_token } = store.getState().auth;
+	if ( access_token ){
+		config.headers.Authorization =` Bearer $ access_token`
+	}
+	config.headers['Access-Control-Allow-Origin'] = '*';
+	return config
+	})
+
+export default instance;
