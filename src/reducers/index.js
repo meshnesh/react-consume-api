205c9191438f781036ba5d74c9274a29e07bd25b
meshnesh <tonnie.nesh@gmail.com>
2018-02-28 20:27:53 +0300

    [Chore] Update imports for user

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from "redux";
 
 import eventsReducer from "./eventsReducer";
-import { registration } from './registration.reducer';
+import  registration  from './registration.reducer';
 
 export default combineReducers({
     eventsReducer,
-    registration
+    auth:registration,
 });
