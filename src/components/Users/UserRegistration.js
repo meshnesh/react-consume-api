d82de69da5fea9b4743e6d055194992a794148c7
meshnesh <tonnie.nesh@gmail.com>
2018-02-28 13:54:46 +0300

    [Feature] Create registration componont for users

--- src/components/Users/UserRegistration.js
+++ src/components/Users/UserRegistration.js
@@ -1,45 +1,69 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 
 //components
 import Navigation from '../Navigation/Navigation';
 import Footer from '../Footer/Footer';
+
+import  userActions  from '../../actions';
 
 
 class UserRegistration extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: '',
-            // email: '',
-            // password: ''
-        }
+           user: {
+               name: '',
+               email: '',
+               password: ''
+            },
+            submitted: false
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
 
     }
 
-    handleChange = (e) => {
-        this.setState({ [e.target.id]: e.target.value })
+    handleChange(event) {
+        const { name, value } = event.target;
+        const { user } = this.state;
+        this.setState({
+            user: {
+                ...user,
+                [name]: value
+            }
+        });
     }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("User registration", this.state);
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.setState({ submitted: true });
+        const { user } = this.state;
+        const { dispatch } = this.props;
+        if (user.name && user.email && user.password) {
+            
+            dispatch(userActions.register(user));
+            console.log("user registratio",user)
+        }
     }
 
     render() {
+        const { registering  } = this.props;
+        const { user, submitted } = this.state;
         return(
             <div>
                 <Navigation />
                 <div className="row rowParent">
-                    <form className="col s8 form" name="form" onSubmit={ this.handleSubmit }>
+                    <form className="col s8 form" name="form" onSubmit={this.handleSubmit}>
 
                         {/* Name form */}
                         <div className="row">
                             <div className="input-field col s12">
-                            <input id="name" type="text" className="validate" value={ this.state.name } onChange={this.handleChange}/>
+                            <input id="name" type="text" name="name" className="validate" value={ user.name } onChange={this.handleChange}/>
                             <label htmlFor="name">Full Names</label>
                             </div>
                         </div>
@@ -47,11 +71,11 @@ class UserRegistration extends Component {
                         {/* Email & Password form */}
                         <div className="row">
                             <div className="input-field col s6">
-                                <input id="email" type="email" className="validate" value={ this.state.email } onChange={ this.handleChange }/>
+                                <input id="email" type="email" name="email" className="validate" value={ user.email } onChange={ this.handleChange }/>
                                 <label htmlFor="email">Email</label>
                             </div>
                             <div className="input-field col s6">
-                                <input id="password" type="password" className="validate" value={ this.state.password } onChange={ this.handleChange }/>
+                                <input id="password" type="password" name="password" className="validate" value={ user.password } onChange={ this.handleChange }/>
                                 <label htmlFor="password">Password</label>
                             </div>
                         </div>
@@ -69,4 +93,12 @@ class UserRegistration extends Component {
     }
 }
 
-export default UserRegistration;
+function mapStateToProps(state) {
+    const { registering } = state.registration;
+    return {
+        registering
+    };
+}
+
+
+export default connect(mapStateToProps)(UserRegistration);
