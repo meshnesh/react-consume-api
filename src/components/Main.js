62e90e11c38c516f14da96caf92fd0cbca37ff34
meshnesh <tonnie.nesh@gmail.com>
2018-02-28 20:14:37 +0300

    [Chore] Remove alert files

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,8 +7,6 @@ import EventList from './Events/EventList';
 import Navigation from './Navigation/Navigation';
 import Footer from './Footer/Footer';
 
-import { history } from '../helpers';
-import { alertActions } from '../actions/alert.actions';
 import { fetchEvents } from '../actions/eventActions';
 
 
@@ -19,16 +17,6 @@ import { fetchEvents } from '../actions/eventActions';
 })
 
 class Main extends Component {
-  constructor(props) {
-      super(props);
-
-      const { dispatch } = this.props;
-      history.listen((location, action) => {
-          // clear alert on location change
-          dispatch(alertActions.clear());
-      });
-  }
-
   componentWillMount(){
 
     this.props.dispatch(fetchEvents());
@@ -39,9 +27,6 @@ class Main extends Component {
     return (
       <div>
         <Navigation />
-        {alert.message &&
-            <div className={`alert ${alert.type}`}>{alert.message}</div>
-        }
         <EventList events ={this.props.events}/>
         <Footer />
       </div>
@@ -49,15 +34,4 @@ class Main extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  const { alert } = state;
-  return {
-      alert
-  };
-}
-
-// const connectedApp = connect(mapStateToProps)(Main);
-// export { connectedApp as Main }; 
-
-
-export default connect(mapStateToProps)(Main);
+export default Main;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,8 @@ import { combineReducers } from "redux";
 
 import eventsReducer from "./eventsReducer";
 import { registration } from './registration.reducer';
-import { alert } from './alert.reducer';
 
 export default combineReducers({
     eventsReducer,
-    registration,
-    alert
+    registration
 });
