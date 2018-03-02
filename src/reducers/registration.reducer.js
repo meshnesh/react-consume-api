99bf443865c0a2769f8525ebec18256abf0cc9df
meshnesh <tonnie.nesh@gmail.com>
2018-03-02 20:45:47 +0300

    [Chore] Update user registration reducers

--- src/reducers/registration.reducer.js
+++ src/reducers/registration.reducer.js
@@ -1,22 +1,33 @@
 export const initialState = {
-  registering:false
-}
- 
-export default (state = initialState, action) => {
-  switch (action.type) {
-    case 'USERS_REGISTER_REQUEST':
-      return { 
-        ...state,
-        registering: true,
-      };
-    case 'REGISTER_USER_FULLIFIED':
-      return {
-        ...state,
-        registering: true,
-        message:action.payload.data.message,
-      };
- 
-    default:
-      return state
+    user:{},
+    registering:false,
+    registered:false,
+    error: null
   }
-}
+  
+  export default (state = initialState, action) => {
+      switch (action.type) {
+        case 'USERS_REGISTER_REQUEST':
+          return { 
+            ...state,
+            registering: true,
+          };
+        case 'USERS_REGISTER_SUCCESS':
+          return {
+            ...state,
+            registering: true,
+            registered:true,
+            user: action.payload,
+          }
+        case 'USERS_REGISTER_FAILURE':
+          return {
+              ...state,
+              registering: false,
+              error: action.payload
+          }
+   
+        default:
+          return state
+      }
+  }
+  
