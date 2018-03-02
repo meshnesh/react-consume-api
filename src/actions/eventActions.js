39377fb79a178ecf3c42938e9bcd7c3b7a6c31c0
meshnesh <tonnie.nesh@gmail.com>
2018-03-02 21:00:02 +0300

    [Chore] Use instance file in 'eventActions'

--- src/actions/eventActions.js
+++ src/actions/eventActions.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import  instance from '../_services/user.service';
 
 export const fetchEvents = () => {
 
   return (dispatch) => {
-    axios.get('https://bright-events.herokuapp.com/api/events/all')
+    instance.get('api/events/all')
         .then( (response) =>{
           dispatch({type:'FETCH_EVENTS_FULFILLED', payload:response.data});
         })
