bcd8b02c1da0990db726bf98776bfad2330f688e
meshnesh <tonnie.nesh@gmail.com>
2018-03-02 20:43:09 +0300

    [Feature] Add user login action && promises

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -6,5 +6,26 @@ export function userActions(user){
     return {
       type : userConstants.REGISTER_REQUEST,
       payload :instance.post('api/auth/register', user)
+      .then((response) => {
+        console.log("user.register.action",response.data);
+      })
+      .catch((err) => {
+        console.log("user.register.action",err);
+      })
     }
+};
+
+
+export function userLoginActions(user){
+  return {
+    type : userConstants.LOGIN_USER_REQUEST,
+    payload :instance.post('api/auth/login', user)
+    .then( (response) => {
+      console.log("user.login.action",response.data);
+    })
+    .catch( (err) => {
+      console.log("user.login.action",err);
+    })
+    
+  }
 };
