5fe7cbaf674f6138cd8088f4a6a6a9203024efda
meshnesh <tonnie.nesh@gmail.com>
2018-03-02 20:43:56 +0300

    [Feature] Update user login component

--- src/components/Users/UserLogin.js
+++ src/components/Users/UserLogin.js
@@ -1,32 +1,48 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 //components
 import Navigation from '../Navigation/Navigation';
 import Footer from '../Footer/Footer';
+
+//actions
+import { userLoginActions } from '../../actions/user.actions';
 
 
 class UserLogin extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: '',
-            password: ''
+            userData: {
+                email: '',
+                password: ''
+             },
+             submitted: false
         }
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange = (e) => {
-        this.setState({ [e.target.id]: e.target.value });
-    }
+    
+    handleChange(e) {
+        const { userData } = this.state;
+        userData[e.target.name] = e.target.value
+        this.setState(userData)
+    };
 
-    handleSubmit = (e) => {
+    handleSubmit(e) {
         e.preventDefault();
-        console.log("User Login", this.state);
-    }
+        const  user = this.state
+        this.props.userLoginActions(user)
+        this.setState({user: ""})
+    };
+
     render() {
+        const { login  } = this.props;
+        const { userData, submitted } = this.state;
+
         return(
             <div>
                 <Navigation />
@@ -36,11 +52,11 @@ class UserLogin extends Component {
                         {/* Email & Password form */}
                         <div className="row">
                             <div className="input-field col s6">
-                                <input id="email" type="email" className="validate" value={this.state.email} onChange={this.handleChange}/>
+                                <input id="email" type="email" name="email" className="validate" value={ userData.email } onChange={this.handleChange}/>
                                 <label for="email">Email</label>
                             </div>
                             <div className="input-field col s6">
-                                <input id="password" type="password" className="validate" value={this.state.password} onChange={this.handleChange}/>
+                                <input id="password" type="password" name="password" className="validate" value={ userData.password} onChange={this.handleChange}/>
                                 <label for="password">Password</label>
                             </div>
                         </div>
@@ -58,4 +74,17 @@ class UserLogin extends Component {
     }
 }
 
-export default UserLogin;
+const mapStateToProps = (state) => {
+    return{
+        userInfo:state.auth
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        userLoginActions: user =>dispatch(userLoginActions(user))
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserLogin);
