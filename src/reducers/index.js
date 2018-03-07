bd65d90e216030db1e5bbafe71a166067670ab84
meshnesh <tonnie.nesh@gmail.com>
2018-03-07 16:04:15 +0300

    [Feature] Implement Reducer for single event

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import eventsReducer from "./eventsReducer";
 import  registration  from './registration.reducer';
+import singleEventReducer from './singleEventReducer'
 
 export default combineReducers({
     eventsReducer,
     auth:registration,
+    event:singleEventReducer
 });
