89e588c587b586ef926d54573770b78a67042a58
meshnesh <tonnie.nesh@gmail.com>
2018-03-07 16:04:42 +0300

    [Feature] Implement Reducer for single event

---
+++ src/reducers/singleEventReducer.js
@@ -0,0 +1,29 @@
+const singleEventReducer = ( 
+    state={
+        event:{},
+        fetching: false,
+        fetched: false,
+        error: null
+    }, action) => {
+        switch (action.type) {
+            case "FETCH_EVENT_ID":
+                return {...state, fetching: true}
+            case "FETCH_EVENT_ID_FULFILLED":
+                return {
+                    ...state,
+                    fetching: false,
+                    fetched: true,
+                    event: action.payload
+                }
+            case "FETCH_EVENT_ID_REJECTED":
+                return {
+                    ...state,
+                    fetching: false,
+                    error: action.payload
+                }
+            default:
+                return state;
+        }
+    }
+
+export default singleEventReducer;
