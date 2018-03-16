90320ee8d1d761d982f5539269813cddb240a2f9
meshnesh <tonnie.nesh@gmail.com>
2018-03-16 03:02:07 +0300

    [Feature] Add more tests in login component

--- src/components/__tests__/Login.test.js
+++ src/components/__tests__/Login.test.js
@@ -12,6 +12,35 @@ describe('Login Component', () => {
  
     // make our assertion and what we expect to happen 
     it('should render without throwing an error', () => {
-      expect(wrapper.find('.col.s8.form').exists()).toBe(true);
+        expect(wrapper.find('.col.s8.form').exists()).toBe(true);
+    })
+
+    it('renders a email input', () => {
+        expect(shallow(<UserLogin />).find('#email').length).toEqual(1)
+    })
+
+    it('renders a password input', () => {
+        expect(shallow(<UserLogin />).find('#password').length).toEqual(1)
+    })
+})
+
+describe('Email input', () => {
+  
+    it('should respond to change event and change the state of the Login Component', () => {
+
+        const wrapper = shallow(<UserLogin />);
+        wrapper.find('#email').simulate('change', {target: {name: 'email', value: 'blah@gmail.com'}});
+        
+        expect(wrapper.state('email')).toEqual('blah@gmail.com');
+    })
+})
+   
+describe('Password input', () => {
+
+    it('should respond to change event and change the state of the Login Component', () => {
+        const wrapper = shallow(<UserLogin />);
+        wrapper.find('#password').simulate('change', {target: {name: 'password', value: 'cats'}});
+
+        expect(wrapper.state('password')).toEqual('cats');
     })
 })
