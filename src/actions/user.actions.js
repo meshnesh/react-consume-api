acbf57766b07c41f28f6e7ec6b30afde4d4ca2ea
meshnesh <tonnie.nesh@gmail.com>
2018-04-03 14:43:30 +0300

    [Feature] Update user login actions

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -1,9 +1,19 @@
-import { userConstants } from '../constants';
-import  instance from '../_services/user.service';
-                                                                       
+import * as types from './actionTypes';
+// import { userConstants } from '../constants';
+import instance from '../_services/user.service';
+import { beginAjaxCall, ajaxCallError } from './AjaxStatusActions';
+import toastr from 'toastr';
+  
+
+export function loginSuccess(payload) {
+  return {
+      type: types.LOGIN_SUCCESS,
+      payload
+  }
+}
 
 export function userActions(user){
-    return {
+  return {
       type : userConstants.REGISTER_REQUEST,
       payload :instance.post('api/auth/register', user)
       .then((response) => {
@@ -16,16 +26,33 @@ export function userActions(user){
 };
 
 
-export function userLoginActions(user){
-  return {
-    type : userConstants.LOGIN_USER_REQUEST,
-    payload :instance.post('api/auth/login', user)
-    .then( (response) => {
-      console.log("user.login.action",response.data);
-    })
-    .catch( (err) => {
-      console.log("user.login.action",err);
-    })
+// export function userLoginActions(user){
+//   return {
+//     type : userConstants.LOGIN_USER_REQUEST,
+//     payload :instance.post('api/auth/login', user)
+//     .then( (response) => {
+//       dispatch(loginSuccess(resp.data));
+//     })
+//     .catch( (err) => {
+//       console.log("user.login.action",err);
+//     })
     
+//   }
+// };
+
+export function userLoginActions(user) {
+  return function (dispatch) {
+      dispatch(beginAjaxCall());
+      return instance
+      .post('api/auth/login', user)
+      .then(resp => {
+          dispatch(loginSuccess(resp.data));
+      })
+      console.log("User Login",response.data)
+      .then(() => toastr.success('You are loged in'))
+      .catch(error => {
+          dispatch(ajaxCallError(error));
+          toastr.error(error.response.data.error)
+      })
   }
-};
+}
