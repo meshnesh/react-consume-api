50fca0680f7dad192dc7ad1a52995ad3257bb2bc
meshnesh <tonnie.nesh@gmail.com>
2018-04-10 13:03:11 +0300

    [Feature] Add thunk and update to set access token to local storage

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -1,51 +1,54 @@
 import * as types from './actionTypes';
-// import { userConstants } from '../constants';
 import instance from '../_services/user.service';
 import { beginAjaxCall, ajaxCallError } from './AjaxStatusActions';
 import toastr from 'toastr';
+import handleAuthErr from './index';
   
 
 export function loginSuccess(payload) {
-  return {
-      type: types.LOGIN_SUCCESS,
-      payload
-  }
+	return {
+		type: types.LOGIN_SUCCESS,
+		payload
+	};
 }
 
 export function registrationSuccess(payload) {
-  return {
-      type: types.REGISTER_SUCCESS,
-      payload
-  }
+	return {
+		type: types.REGISTER_SUCCESS,
+		payload
+	};
 }
 
-export function userActions(user) {
-  return function (dispatch) {
-    dispatch(beginAjaxCall());
-    return instance.post('api/auth/register', user)
-    .then(resp => {
-      dispatch(registrationSuccess(resp.data));
-      toastr.success(resp.data.message);
-    })
-    .catch(error => {
-      dispatch(ajaxCallError(error));
-      toastr.error(error.response.data.error);
-    });
-  }
+export function userRegistrationActions(user) {
+	return function (dispatch) {
+		dispatch(beginAjaxCall());
+		return instance
+			.post('api/auth/register', user)
+			.then(resp => {
+				dispatch(registrationSuccess(resp.data));
+				toastr.success(resp.data.message);
+			})
+			.catch(err => {
+				dispatch(handleAuthErr('signup', err.response.status));
+				toastr.error(err);
+			});
+	};
 }
 
 export function userLoginActions(user) {
-  return function (dispatch) {
-      dispatch(beginAjaxCall());
-      return instance
-      .post('api/auth/login', user)
-      .then(resp => {
-          dispatch(loginSuccess(resp.data));
-          toastr.success(resp.data.message);
-      })
-      .catch(error => {
-          dispatch(ajaxCallError(error));
-          toastr.error(error.response.data.error)
-      })
-  }
+	return function (dispatch) {
+		dispatch(beginAjaxCall());
+		return instance
+			.post('api/auth/login', user)
+			.then(resp => {
+				let { access_token, message } = resp.data; 
+				dispatch(loginSuccess(resp.data));
+				toastr.success(message);
+				localStorage.setItem('access_token', access_token);
+			})
+			.catch(error => {
+				dispatch(ajaxCallError(error));
+				toastr.error(error.response.data.error);
+			});
+	};
 }
