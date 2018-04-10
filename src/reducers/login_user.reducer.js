fd7e88a195a59a1946c7ea91cb5d440eed4c54c5
meshnesh <tonnie.nesh@gmail.com>
2018-04-10 13:30:53 +0300

    [Chore] Update initial state and state manangment after login

--- src/reducers/login_user.reducer.js
+++ src/reducers/login_user.reducer.js
@@ -2,24 +2,33 @@ import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
 
-export default (state = initialState.auth, action) => {
-    switch (action.type) {
-        case types.LOGIN_SUCCESS:
-            return { 
-            ...state,
-            loading: true,
-            token: action.payload.token,
-            message: action.payload.message,
-            authenticated: true
-            }
+export default (state = initialState.authLogin, action) => {
+	switch (action.type) {
+	case types.LOGIN_SUCCESS:
+		return { 
+			...state,
+			loading: true,
+			token: action.payload.token,
+			authenticated: true,
+		};
 
-        case types.SHOW_LOGIN:
-            return {
-            ...state,
-            login: true,
-            }
+	case types.SHOW_LOGIN:
+		return {
+			...state,
+			login: true,
+		};
 
-        default:
-            return state
-    }
-}
+	case types.REGISTER_SUCCESS:
+		return { 
+			...state,
+			loading: true,
+			login:false,
+			message: action.payload.message,
+			authenticated: true,
+			registered:true,
+		};
+
+	default:
+		return state;
+	}
+};
