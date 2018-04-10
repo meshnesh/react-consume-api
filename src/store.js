bfc774066e6d76cc5926bbf5ae8e42feb0932241
meshnesh <tonnie.nesh@gmail.com>
2018-04-10 12:56:31 +0300

    [Chore] Add redux debugging extension code

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
-import {applyMiddleware, createStore}  from "redux";
+import {applyMiddleware, createStore}  from 'redux';
 
 //middlewares
-import logger from "redux-logger";
-import promise from "redux-promise-middleware";
-import thunk from "redux-thunk";
+import logger from 'redux-logger';
+import promise from 'redux-promise-middleware';
+import thunk from 'redux-thunk';
 
-import reducers from "./reducers/index";
+import reducers from './reducers/index';
 
 const middleware = applyMiddleware(promise(), thunk, logger());
 
-export default createStore(reducers, middleware);
+const extenssion = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+export default createStore(reducers,extenssion,middleware);
