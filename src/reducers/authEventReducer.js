599ddbf4ef88369d4f1779ad17ee7ffaa6ced73f
meshnesh <tonnie.nesh@gmail.com>
2018-04-10 13:27:31 +0300

    [Feature] Create file for getting authenticated user events

---
+++ src/reducers/authEventReducer.js
@@ -0,0 +1,13 @@
+import * as types from '../actions/actionTypes';
+import initialState from './initialState';
+
+
+export default (state = initialState.events, action) => {
+	switch (action.type) {
+	case types.GET_AUTH_EVENT_SUCCESS:
+		return action.payload;
+    
+	default:
+		return state;
+	}
+};
