fa66346012e1d2a3a32e3abf0b498be1b9abe3f6
meshnesh <tonnie.nesh@gmail.com>
2018-04-24 21:26:26 +0300

    [Feature] Add action type for editing events

--- src/reducers/authEventReducer.js
+++ src/reducers/authEventReducer.js
@@ -6,6 +6,9 @@ export default (state = initialState.events, action) => {
 	switch (action.type) {
 	case types.GET_AUTH_EVENT_SUCCESS:
 		return action.payload;
+
+	case types.EDIT_EVENT_SUCCESS:
+		return action.payload;
     
 	default:
 		return state;
