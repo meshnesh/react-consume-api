66bb6a32ba38fe7fd3c2be47ec1ff0f4647ece80
meshnesh <tonnie.nesh@gmail.com>
2018-04-24 21:25:28 +0300

    [Feature] Implement functionality to edit single event gotten by id

--- src/components/AuthEvents/EditEvent.js
+++ src/components/AuthEvents/EditEvent.js
@@ -5,7 +5,8 @@ import { connect } from 'react-redux';
 import Navigation from '../Navigation/Navigation';
 import Footer from '../Footer/Footer';
 
-import  { getEventDetail } from '../../actions/eventActions';
+import { getEventDetail } from '../../actions/eventActions';
+import { editEventAction } from '../../actions/authEvents.actions';
 
 
 const initialState = {
@@ -22,70 +23,83 @@ const initialState = {
 class EditEvent extends Component{
 
     constructor(props){
-        super(props);
+		super(props);
         this.state = {
-			eventData: initialState,
-			submitted: false
+			eventData: initialState
 		};
-        this.handleInputChange = this.handleInputChange.bind(this)
-    }
+		// console.log('Constructor',editEvent);
+		this.handleInputChange = this.handleInputChange.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	componentWillReceiveProps(nextProps){
+		console.log('componentWillReceiveProps',nextProps)
+		this.setState({eventData: nextProps.editEvent.event})
+	}
 
     componentWillMount = () => {
 		let eventId = this.props.match.params.id
-        this.props.dispatch(getEventDetail(eventId))
-        // console.log("Component will mount", this.props)
-        console.log('componennt', this.state)
+        this.props.getEventAction(eventId)
     }
     
     handleInputChange(e) {
-        // console.log("handleInputChange", this.props.editEvent.event)
         const { eventData } = this.state;
         eventData[e.target.name] = e.target.value;
-        // console.log('handle Input', this.state);
 		this.setState(eventData);
-    }
+	}
+
+
+	handleSubmit(e) {
+		e.preventDefault();
+		const event = this.state.eventData;
+		let eventId = this.props.match.params.id
+		console.log('handleSubmit', this.props);
+		this.props.editEventAction(eventId, event);
+		this.setState({eventData: initialState});
+	}
     
 	render() {
-        console.log("From the edit event ",this.props.editEvent.event);
-        const eventData = this.props.editEvent.event;
+
+		const { eventData } = this.state
+		
 		return(
 			<div>
 				<Navigation />
 				<div className="row">
-					<form className="col s12">
+					<form className="col s12" onSubmit={ this.handleSubmit } >
 						<div className="row">
 							<div className="input-field col s12">
-								<input id="title" type="text" name="title" className="validate" value={ eventData.title } onChange={this.handleInputChange} />
+								<input id="title" type="text" name="title" className="validate" value={ eventData.title } onChange={ this.handleInputChange } />
 								<label class="active" for="title">Event Title</label>
 							</div>
 						</div>
 						<div className="row">
 							<div className="input-field col s6">
-								<input id="location" type="text" name="location" className="validate" value={ eventData.location } />
+								<input id="location" type="text" name="location" className="validate" value={ eventData.location } onChange={ this.handleInputChange } />
 								<label class="active" for="location">Event Location</label>
 							</div>
 							<div className="input-field col s6">
-								<input id="image_url" type="text" name="image_url" className="validate" value={ eventData.image_url } />
+								<input id="image_url" type="text" name="image_url" className="validate" value={ eventData.image_url } onChange={ this.handleInputChange } />
 								<label class="active" for="image_url">Image url</label>
 							</div>
 						</div>
 						<div className="row">
 							<div className="input-field col s4">
-								<input id="date" type="text" name="date" className="validate" value={ eventData.date } />
+								<input id="date" type="text" name="date"className="validate" value={ eventData.date } onChange={ this.handleInputChange } />
 								<label class="active" for="date">Date</label>
 							</div>
 							<div className="input-field col s4">
-								<input id="time" type="text" name="time" className="validate" value={ eventData.time } />
+								<input id="time" type="text" name="time" className="validate" value={ eventData.time } onChange={ this.handleInputChange } />
 								<label class="active" for="time">Time</label>
 							</div>
 							<div className="input-field col s4">
-								<input id="event_category" type="text" name="event_category" className="validate" value={ eventData.event_category } />
+								<input id="event_category" type="text" name="event_category" className="validate" value={ eventData.event_category } onChange={ this.handleInputChange } />
 								<label class="active" for="event_category">Event Category</label>
 							</div>
 						</div>
 						<div className="row">
 							<div className="input-field col s12">
-								<textarea id="description" name="description" className="materialize-textarea" value={ eventData.description } ></textarea>
+								<textarea id="description" name="description" className="materialize-textarea" value={ eventData.description } onChange={ this.handleInputChange } ></textarea>
 								<label class="active" for="description">Event Description</label>
 							</div>
 						</div>
@@ -106,4 +120,10 @@ const mapStateToProps = (state) => {
 	}
 }
 
-export default connect(mapStateToProps)(EditEvent);
+const mapDispatchToProps = (dispatch) =>  ({
+	getEventAction: (eventId) => dispatch(getEventDetail(eventId)),
+	editEventAction: (eventId, event) => dispatch(editEventAction(eventId, event))
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditEvent);
