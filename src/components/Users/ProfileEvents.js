8c92c1d744716dfddb65f01fdbca3ba2363b1b84
meshnesh <tonnie.nesh@gmail.com>
2018-06-04 12:19:31 +0300

    [Chore] fix merge conflicts

--- src/components/Users/ProfileEvents.js
+++ src/components/Users/ProfileEvents.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 const ProfileEvent = (props) => (
-	<div>
-		<div className="collection">
-			<div className="collection-item" id={ props.id }>
-				<h4>{ props.title }</h4>
-				<span>{ props.date } at { props.time }</span> <span className="event-rsvp">0 RSVP</span>
-				<ul className="manage_tickets">
-					<li>
-						<a> 
-							<i className="fas fa-cogs"></i> Manage
-						</a>
-					</li>
-					<li>
-						<a>
+	<div className="collection">
+		<div className="collection-item" id={ props.id }>
+			<h4>{ props.title }</h4>
+			<span>{ props.date } at { props.time }</span> <span className="event-rsvp">0 RSVP</span>
+			<ul className="manage_tickets">
+				<li>
+					<a>
+						<i className="fas fa-cogs"></i> Manage
+					</a>
+				</li>
+				<li>
+					<a>
+						<Link to={`/edit/${props.id}`}>
 							<i className="far fa-edit"></i> Edit
-						</a>
-					</li>
-					<li>
-						<a>
-							<i className="fas fa-eye"></i> View
-						</a>
-					</li>
-				</ul>
-			</div>
+						</Link>
+					</a>
+				</li>
+				<li>
+					<a>
+						<i className="fas fa-eye"></i> View
+					</a>
+				</li>
+			</ul>
 		</div>
 		<Link to="/addevent">
 			<a className="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
