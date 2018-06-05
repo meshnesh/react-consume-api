e29584fd0304364d8c89d5e612bc609343547418
meshnesh <tonnie.nesh@gmail.com>
2018-06-05 14:35:09 +0300

    [Chore] fix user profile issue with code

--- src/components/Users/UserProfile.js
+++ src/components/Users/UserProfile.js
@@ -84,7 +84,7 @@ class UserProfile extends Component {
 
 const mapStateToProps = (state) => {
 	return {
-		authenticatedEvent: state.authEvent.events
+		authenticatedEvent: state.authEvent[0].events
 	};
 	
 };
