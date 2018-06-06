1918b9f0dd952c48945b4c9f4c9b491ce319801e
meshnesh <tonnie.nesh@gmail.com>
2018-06-06 15:01:18 +0300

    [Chore] Change search component to functional

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider } from 'react-redux';
-
+import { loginSuccess } from './actions/user.actions';
 // main routes
 import AppRoutes from './routes';
 
@@ -12,7 +12,16 @@ import store from './store.js';
 
 // dispatch action that will hyrate from local storage
 
+function hydrateStore(){
+	const access_token = localStorage.getItem('access_token');
 
+	if(access_token) {
+		store.dispatch(loginSuccess({token: access_token}));
+	}
+
+}
+
+hydrateStore();
 ReactDOM.render(
 	<MuiThemeProvider>
 		<Provider store={ store }>
