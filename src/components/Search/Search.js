96726ffd84b089027aa363a7bd1e835a0f5f63ba
meshnesh <tonnie.nesh@gmail.com>
2018-06-06 12:13:54 +0300

    [Chore] Change search component to functional

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,24 +2,22 @@ import React, { Component } from 'react';
 
 import Category from '../category/GetCategories';
 
-class Search extends Component {
-	render() {
-		return (
-			<div className="row search">
-				<form className="col s12">
-					<div className="row">
-						<div className="input-field col s6">
-							<input placeholder="Location" id="Location" type="text" className="validate" />
-							<label for="Location">Location</label>
-						</div>
-						<div className="input-field col s12 m6">
-							<Category handleCategoryChange={this.handleCategoryChange} />
-						</div>
+const Search = (props) => {
+	return (
+		<div className="row search">
+			<form className="col s12">
+				<div className="row">
+					<div className="input-field col s6">
+						<input placeholder="Location" id="Location" type="text" className="validate" onChange={props.onChange}/>
+						<label for="Location">Location</label>
 					</div>
-				</form>
-			</div>
-		);
-	}
-}
+					<div className="input-field col s12 m6">
+						<Category />
+					</div>
+				</div>
+			</form>
+		</div>
+	);
+};
 
 export default Search;
