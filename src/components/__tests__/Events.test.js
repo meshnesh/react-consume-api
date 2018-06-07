d2a3bce35ddfdce12e9997b7107294439aa864b7
meshnesh <tonnie.nesh@gmail.com>
2018-06-07 12:50:58 +0300

    [Feature] Add test for search component

--- src/components/__tests__/Events.test.js
+++ src/components/__tests__/Events.test.js
@@ -2,18 +2,33 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import Events from '../Events/EventItem';
+import Search from '../Search/Search';
 
 
 // describe what we are testing
 describe('Events Component', () => {
-    let wrapper;
-    beforeEach(() => {
-        wrapper = shallow(<Events />)
-    })
+	let wrapper;
+	beforeEach(() => {
+		wrapper = shallow(<Events />);
+	});
  
-    // make our assertion and what we expect to happen 
-    it('should render without throwing an error', () => {
-        expect(wrapper.find('.col.s12.m4').exists()).toBe(true);
-    })
-})
+	// make our assertion and what we expect to happen 
+	it('should render without throwing an error', () => {
+		expect(wrapper.find('.col.s12.m4').exists()).toBe(true);
+    });
+    
+});
 
+// describe what we are testing
+describe('Search Component', () => {
+	let wrapper;
+	beforeEach(() => {
+		wrapper = shallow(<Search />);
+	});
+ 
+	// make our assertion and what we expect to happen 
+	it('should render without throwing an error', () => {
+		expect(wrapper.find('.row .search').exists()).toBe(true);
+    });
+    
+});
