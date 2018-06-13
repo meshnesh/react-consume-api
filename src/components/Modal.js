558f119bc4f08b547ba28096662be0a09bc34241
meshnesh <tonnie.nesh@gmail.com>
2018-06-13 12:11:12 +0300

    [Chore] Update icon for the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ModalBUtton = (props) => (
 	<div>
 		<a className="waves-effect waves-light btn modal-trigger" onClick={() => props.toggleModal(props.id)}>
-			<i className="fas fa-eye"></i> Modal
+			<i className="fas fa-trash"></i> Modal
 		</a>
 	</div>
 	
