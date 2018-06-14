df778eaf20412febe9d5aa4bbaca9cbdf2cf8830
meshnesh <tonnie.nesh@gmail.com>
2018-06-14 15:47:08 +0300

    [Feature] Add code for moving to the next page

--- src/components/Events/EventList.js
+++ src/components/Events/EventList.js
@@ -3,14 +3,32 @@ import React, { Component } from 'react';
 import Event from './EventItem';
 import Search from '../Search/Search';
 
+import { fetchEvents } from '../../actions/eventActions';
 
 class EventList extends Component {
 	state = {
-        search: ''
+		search: '',
+		page: 1
 	}
 	
 	updateSearch = event => {
         this.setState({ search: event.target.value});
+	}
+
+	nextPage = () => {
+		const page = this.state.page
+		const limit = 2 
+		console.log('nextPage', this.props)
+		this.setState({page: page + 1}, () => this.props.dispatch(fetchEvents(limit, page)));
+	}
+
+	prevPage = () => {
+		const page = this.state.page
+		const limit = 2 
+		if(page > 1){
+			return this.setState({page: page - 1}, () => this.props.dispatch(fetchEvents(limit, page)));
+		}
+		console.log('could not decreent');
 	}
 	
 	render(){
@@ -22,30 +40,42 @@ class EventList extends Component {
 
 		let eventNodes = [];
 
-		if (filteredEvents.length !== 0 ) {
-			eventNodes = filteredEvents.map( ( event ) => {
-				return (
-					<Event key={ event.id } id={event.id} image_url={ event.image_url } title={ event.title } location={ event.location }
-						date={ event.date } time={ event.time } description={ event.description }
-						event_category={ event.event_category } />
-				);
-			})
-		}
-		else {
+		eventNodes = filteredEvents.map( ( event ) => {
 			return (
-				<div className="row">
-					<h1 className="event-List"></h1>
-					<Search onChange={this.updateSearch} />
-					<h1 className="center">No Events</h1>
-				</div>
+				<Event key={ event.id } id={event.id} image_url={ event.image_url } title={ event.title } location={ event.location }
+					date={ event.date } time={ event.time } description={ event.description }
+					event_category={ event.event_category } />
+			);
+		})
 
-			)
-		}
+
+		const event = filteredEvents.length === 0 ? (
+			<h1 className="center">No Events</h1>
+		): (
+			{eventNodes}
+		);
+
 		return (
 			<div className="row">
 				<h1 className="event-List"></h1>
 				<Search onChange={this.updateSearch} />
 				{eventNodes}
+				<div class="row">
+				<div class="col s12 center">
+					<div class="col m6 center">
+						<ul class="pagination">
+							<li class="waves-effect" onClick={this.prevPage}><a ><i class="material-icons">chevron_left</i></a></li>
+						</ul>
+									
+					</div>
+					<div class="col s6 center">
+						<ul class="pmgination">
+							<li class="waves-effect" onClick={this.nextPage}><a ><i class="material-icons">chevron_right</i></a></li>
+						</ul>
+					</div>
+					</div>
+				</div>
+
 			</div>
 		);
 	}
