b7613c86dcd8f137540eff72ce9e4c50a2093fab
Antony <tonnie.nesh@gmail.com>
2018-06-17 14:29:17 +0300

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,54 +2,72 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { LoginButton, LogoutButton } from './LoginButton';
-import { userLogoutActions, userLoginActions } from '../../actions/user.actions';
 
+
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import ViewModule from 'material-ui/svg-icons/action/view-module';
+import {white} from 'material-ui/styles/colors';
+
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import { userLogoutActions, userLoginActions } from '../../actions/user.actions';
 
 class Nav extends Component {
 
 	constructor(props) {
 		super(props);
 		this.handleLogoutClick = this.handleLogoutClick.bind(this);
-		this.state = {isLoggedIn: this.props.userInfo.isLoggedIn};
+		this.state = {
+			isLoggedIn: this.props.userInfo.isLoggedIn
+		};
 	}
 
-	
+
 	handleLogoutClick() {
 		this.setState({isLoggedIn: false});
 		this.props.userLogoutActions();
 	}
 
+
 	render() {
+
 		const isLoggedIn = this.state.isLoggedIn;
-    
+
 		const button = isLoggedIn ? (
-			<LogoutButton onClick={this.handleLogoutClick} />
-		) : (
-			<LoginButton>
-				<Link to="/register">
-					Register
+			<div>
+				<Link to="/newProfile">
+					<MenuItem key={1} primaryText="Profile"/>
 				</Link>
-			</LoginButton>
+
+				<MenuItem primaryText="Sign out" onClick={this.handleLogoutClick} />
+			</div>
+		) : (
+			<Link to="/Login">
+				<MenuItem key={1} primaryText="Login"/>
+			</Link>
 		);
+
 
 		return (
 			<nav>
 				<div className="nav-wrapper">
-
 					<Link className="brand-logo" to="/">
                         Bright Events
 					</Link>
                     
 					<ul id="nav-mobile hide-on-small-only" className="right hide-on-med-and-down">
-						<li>
-							{button}
-						</li>
-						<li>
-							<Link to="/profile">
-                                Manage Events
-							</Link>
-						</li>
+
+						<IconMenu color={white}
+							iconButtonElement={
+								<IconButton><MoreVertIcon color={white}/></IconButton>
+							}
+							targetOrigin={{horizontal: 'right', vertical: 'top'}}
+							anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+						>
+							{ button }
+						</IconMenu>
 					</ul>
 				</div>
 			</nav>
